e3c93dbc3b740a72904f82bcc1b4c7c36e1df877
Askill <ofjokg@gmail.com>
2024-07-16 19:23:27 +0200

    added lifecycle for ecr

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -56,3 +56,27 @@ resource "aws_ecr_repository_policy" "optar" {
   repository = aws_ecr_repository.optar.name
   policy     = data.aws_iam_policy_document.optar.json
 }
+
+resource "aws_ecr_lifecycle_policy" "optar_lifecycle" {
+  repository = aws_ecr_repository.optar.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v0"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
